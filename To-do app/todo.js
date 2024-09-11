a50f947517183a9f86fbb0b4f4e6b34dc132008c
n-kaushik1 <nikanshkashik@gmail.com>
2024-09-11 15:52:53 +0530

    to do app updated

--- To-do app/todo.js
+++ To-do app/todo.js
@@ -1,6 +1,4 @@
-let todolist =[
-    {item:'Buy milk',duedate:'4/10/2024'},
-    {item:'goto',duedate:'4/10/2024'}];
+let todolist =[];
 display();
 
 function addtodo(){
@@ -8,7 +6,10 @@ function addtodo(){
    let todoItem =inputelment.value;
    let inputdate = document.querySelector('#date');
    let tododate =inputdate.value;
-   todolist.push({item:todoItem,duedate:tododate});
+   if(inputelment.value!=='' || inputdate.value!==''){
+    todolist.push({item:todoItem,duedate:tododate});
+    localStorage.setItem('todolist',JSON.stringify(todolist));
+   }
    inputelment.value='';
    inputdate.value='';
    display();
@@ -16,20 +17,29 @@ function addtodo(){
 
 function display() {
     let displayelement = document.querySelector('.list');
-
+    let newlist = JSON.parse(localStorage.getItem('todolist'));
     let newHtml = '';
-    
-    for(let i=0;i<todolist.length;i++){
-       let item = todolist[i].item;
-       let duedate = todolist[i].duedate; 
+    if (newlist!==null){   
+     for(let i=0;i<newlist.length;i++){
+       let item = newlist[i].item;
+       let duedate = newlist[i].duedate; 
        newHtml+=`
-       
         <span>${item}</span>
         <span>${duedate}</span>
-        <button class="btn-del" onclick="todolist.splice(${i},1);display();">Delete</button>
+        <button class="btn-del" onclick="del(${i});">Delete</button>
         `;
         
     }
+    }
     displayelement.innerHTML=newHtml;
+}
+function del(i) {
+    todolist.splice(i, 1);
+    localStorage.setItem('todolist', JSON.stringify(todolist));
+    display();
+}
 
+function clearall(){
+    localStorage.clear();
+    display();
 }
